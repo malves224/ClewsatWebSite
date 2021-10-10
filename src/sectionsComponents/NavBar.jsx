270dae93934a58d0ed46f02eaea64b2b23398cb3
Matheus Alves <malves224@gmail.com>
2021-10-10 19:50:05 -0300

    adiciona logica para acionar overlay no componenete NavBar

--- src/sectionsComponents/NavBar.jsx
+++ src/sectionsComponents/NavBar.jsx
@@ -1,20 +1,39 @@
+/* eslint-disable no-unneeded-ternary */
 import React, { Component } from "react";
 import "./css/navBar.css";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+
 import imgLogo from "../img/logo-white-tracker.svg";
 
 class NavBar extends Component {
   constructor() {
     super();
     this.state = {
-      // menu: false,
+      menu: false,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { menu } = this.state;
+    const boolForInsert = menu ? false : true;
+    this.setState({
+      menu: boolForInsert,
+    });
   }
 
   render() {
+    const { menu } = this.state;
     return (
       <nav>
         <img src={imgLogo} alt="imagem do logo da empresa." />
-        <button type="button">menu</button>
+        <button name="menu" id="menu" onClick={this.handleClick} type="button">
+          { !menu ? <FontAwesomeIcon icon={faBars} />
+            : <FontAwesomeIcon icon={faTimes} />}
+        </button>
+        { menu && console.log("renderOverLay")}
       </nav>
     );
   }
