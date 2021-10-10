a2d9111045d115fb14a82b337a39de8798975ff1
Matheus Alves <malves224@gmail.com>
2021-10-10 13:24:40 -0300

    adiciona textos para componente googleEvidence

--- src/textData.js
+++ src/textData.js
@@ -15,24 +15,25 @@ export const differences = {
 	title: `Só pra quem tem`, // sepera o nome clewsa para aplica cor
 	description: 'A Clewsat rastreamento é uma empresa focado na segurança dos clientes.',
 	cardsDifferences: [{
-		icon: 'fas fa-map-marked-alt',
-		title: 'Rastreamento em tempo real.',
-		description: 'Saiba onde está seu veiculo em tempo real acessando pelo app.',
-	}, {
-		icon: 'fas fa-unlock-alt',
-		title: 'Bloqueador pelo app',
-		description: 'Bloqueie seu veiculo pelo aplicativo, de qualquer lugar do mundo.',
-	},
-	{
-		icon: 'fas fa-user-secret',
-		title: 'Equipe de busca',
-		description: 'Conte com uma equipe de busca para recuperação do seu veiculo com segurança',
-	},
-	{
-		icon: 'fas fa-car-crash',
-		title: 'Reboque para imprevistos',
-		description: 'Imprevistos acontece, por isso temos a opção de adicionar assistência com guincho em seu plano',
-	}]
+			icon: 'fas fa-map-marked-alt',
+			title: 'Rastreamento em tempo real.',
+			description: 'Saiba onde está seu veiculo em tempo real acessando pelo app.',
+		}, {
+			icon: 'fas fa-unlock-alt',
+			title: 'Bloqueador pelo app',
+			description: 'Bloqueie seu veiculo pelo aplicativo, de qualquer lugar do mundo.',
+		},
+		{
+			icon: 'fas fa-user-secret',
+			title: 'Equipe de busca',
+			description: 'Conte com uma equipe de busca para recuperação do seu veiculo com segurança',
+		},
+		{
+			icon: 'fas fa-car-crash',
+			title: 'Reboque para imprevistos',
+			description: 'Imprevistos acontece, por isso temos a opção de adicionar assistência com guincho em seu plano',
+		}
+	]
 }
 
 export const appTech = {
@@ -41,4 +42,29 @@ export const appTech = {
 	subtitle: 'O Aplicativo é bem simples de utilizar e intuitivo.',
 	mainFunctionText: 'Principais funções do aplicativo:',
 	mainFunctions: ['tenha acesso a localização exata do seu veiculo.', 'Segurança na palma da mão.', 'Tenha controle total sobre o seu veiculo.'],
+}
+
+export const googleEvidence = {
+	title: 'Depoimentos google',
+	depositions: [{
+			namePeople: 'Vanderlei Picoli',
+			comment: 'Excelente pessoal da Clewsat, Já recomendei pra alguns amigos. Valeu obrigado'
+		},
+		{
+			namePeople: 'Elias silva',
+			comment: 'Rastreamento funcina muito bem ! Atendimento bom imediato....recomendo'
+		},
+		{
+			namePeople: 'Ricardo Oliveira',
+			comment: 'Uma excelente empresa já estou com eles um tempo super indico....'
+		},
+		{
+			namePeople: 'Priscila Batista',
+			comment: 'Sem palavras atenção 100% , super recomendo, além de um ótimo atendimento , o rastreamento tudo OK .'
+		},
+		{
+			namePeople: 'Jonatas Souza Mendes',
+			comment: 'Ótimo atendimento , rastreador funciona perfeitamente , e um ótimo suporte .'
+		}
+	]
 }
