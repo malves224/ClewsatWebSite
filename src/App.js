6187d0b0654cd8f9e79e4b8405032b63d1feca8d
Matheus Alves <malves224@gmail.com>
2021-10-10 16:34:03 -0300

    add componente NavBar e o seu css a fim de teste

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import NavBar from './sectionsComponents/NavBar';
 
 function App() {
   return (
     <div className="App">
       <div>
         <p>Eu sou o app</p>
+        <NavBar />
       </div>
     </div>
   );
---
+++ src/sectionsComponents/NavBar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/navBar.css';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <p className='teste'> oi eu sou seu navBar </p>
+      </div>
+    )
+  }
+}
+
+export default NavBar
