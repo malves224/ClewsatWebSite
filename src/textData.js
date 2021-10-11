2f336214912115e90f88b4f67d53d1b531d69e8a
Matheus Alves <malves224@gmail.com>
2021-10-11 14:17:18 -0300

    correçao textos do card carCrash

--- src/textData.js
+++ src/textData.js
@@ -34,12 +34,12 @@ const differences = {
   {
     icon: "faUserSecret",
     title: "Equipe de busca",
-    description: "Conte com uma equipe de busca para recuperação do seu veiculo com segurança",
+    description: "Conte com uma equipe de busca para recuperação do seu veiculo com segurança.",
   },
   {
     icon: "faCarCrash",
     title: "Reboque para imprevistos",
-    description: "Imprevistos acontece, por isso temos a opção de adicionar assistência com guincho em seu plano",
+    description: "Problema com o veiculo? basta solicitar um guincho.",
   },
   ],
 };
