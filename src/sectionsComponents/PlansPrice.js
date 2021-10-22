a93a69386b79ab41757c69abb25374b2262ccfed
Matheus Alves <malves224@gmail.com>
2021-10-22 17:51:40 -0300

    cria estilizaçao para o plano LIte e condicional das classes

--- src/sectionsComponents/PlansPrice.js
+++ src/sectionsComponents/PlansPrice.js
@@ -1,16 +1,18 @@
-import React, { PureComponent } from "react";
+/* eslint-disable react/jsx-filename-extension */
+import React from "react";
 import "./css/plansPrice.css";
 import { plansPrices } from "../textData";
 import PlanCard from "./components/PlanCard";
 
-class PlansPrice extends PureComponent {
-  render() {
-    return (
-      <div>
+function PlansPrice() {
+  return (
+    <section className="plans-price">
+      <h2>{plansPrices.title}</h2>
+      <div className="carrousel-plans">
         {plansPrices.plansCard.map((plan) => <PlanCard key={plan.plan} plan={plan} />)}
       </div>
-    );
-  }
+    </section>
+  );
 }
 
 export default PlansPrice;
--- src/sectionsComponents/components/PlanCard.jsx
+++ src/sectionsComponents/components/PlanCard.jsx
@@ -1,25 +1,44 @@
 import React, { PureComponent } from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTimes, faCheck,
+} from "@fortawesome/free-solid-svg-icons";
+
 import PropTypes from "prop-types";
+import Button from "./Button";
+import { buttonHireNow } from "../../textData";
 import "../css/planCard.css";
 
 export default class PlanCard extends PureComponent {
   render() {
     const {
       plan: {
-        plan, price, recurrence, benefits,
+        plan, price, recurrence, benefits, promotional,
       },
     } = this.props;
-    console.log(benefits(plan));
+    // console.log(benefits(plan));
     return (
       <div className="item-plan">
-        <header className="header-plan">
+        <header className={promotional.status ? "header-promotional" : "header-no-promotional"}>
           <h3>{plan}</h3>
           <div className="price-plan">
             <h4>{price}</h4>
             <p>{recurrence}</p>
           </div>
         </header>
+        <body className="body-item">
+          <ol className="list-benefits">
+            {benefits(plan)
+              .map(({ textBenefits, status }) => (
+                <li className={status ? "benefit-check benefit" : "benefit-No benefit"}>
+                  <FontAwesomeIcon icon={status ? faCheck : faTimes} />
+                  {" "}
+                  {textBenefits}
+                </li>
+              ))}
+          </ol>
+          <Button src={buttonHireNow.src} text={buttonHireNow.text} />
+        </body>
       </div>
     );
   }
