977201c527a9c126aed97e36c57129ea7fa1e938
Matheus Alves <malves224@gmail.com>
2021-10-23 14:22:11 -0300

    adiciona botoes proximo e previous

--- src/sectionsComponents/PlansPrice.js
+++ src/sectionsComponents/PlansPrice.js
@@ -1,5 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import "./css/plansPrice.css";
 import { plansPrices } from "../textData";
 import PlanCard from "./components/PlanCard";
@@ -7,9 +9,33 @@ import PlanCard from "./components/PlanCard";
 function PlansPrice() {
   return (
     <section className="plans-price">
-      <h2>{plansPrices.title}</h2>
-      <div className="carrousel-plans">
-        {plansPrices.plansCard.map((plan) => <PlanCard key={plan.plan} plan={plan} />)}
+      <header className="header-plans">
+        <h2>{plansPrices.title}</h2>
+        <p>Escolha seu plano</p>
+        <div className="blackfriday-text">
+          <h3>BLACK FRIDAY</h3>
+          <p id="blackfriday-p">DE VERDADE</p>
+        </div>
+      </header>
+      <div className="carrousel-content">
+        <button
+          onClick={() => console.log("oi")}
+          type="button"
+        >
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+
+        <div className="carrousel-plans">
+          {plansPrices.plansCard.map((plan) => <PlanCard key={plan.plan} plan={plan} />)}
+        </div>
+
+        <button
+          onClick={() => console.log("oi")}
+          type="button"
+        >
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+
       </div>
     </section>
   );
--- src/sectionsComponents/components/PlanCard.jsx
+++ src/sectionsComponents/components/PlanCard.jsx
@@ -4,7 +4,6 @@ import {
   faTimes, faCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
-import imgBlackFriday from "../../img/selo-blackFriday.svg";
 import Button from "./Button";
 import { buttonHireNow } from "../../textData";
 import "../css/planCard.css";
@@ -20,15 +19,21 @@ export default class PlanCard extends PureComponent {
     return (
       <section className="item-plan">
         <header className={status ? "header-promotional" : "header-no-promotional"}>
-          <div className={status ? "blackFriday" : ""}>
-            <h3>{plan}</h3>
-            {status && <img src={imgBlackFriday} alt="selo black Frydai" />}
-          </div>
+          <h3>{plan}</h3>
           <div className={status ? "price-current-content" : ""}>
             <h4 className={status ? "price-previous" : ""}>{price}</h4>
-            {status && <span className={status ? "price-current" : ""}>{pricePromotional}</span> }
+            {status
+            && (
+              <span className={status ? "price-current" : ""}>{pricePromotional}</span>
+            ) }
             <p>{recurrence}</p>
           </div>
+          {status
+          && (
+          <div className={status ? "no-installation" : ""}>
+            <p>Sem taxa de instalação</p>
+          </div>
+          )}
         </header>
         <article className="body-item">
           <ol className="list-benefits">
